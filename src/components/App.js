5a25326f0b1dae8a41cb54840a1091eeaea8062c
Rogelio Martin Corsino <rcorsino@gmail.com>
2018-05-16 17:58:16 -0300

    Router add
    add the react router v4

---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topics = () => (
+  <div>
+  <h2>Topics</h2>
+</div>
+)
+
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/topics">Topics</Link></li>
+        </ul>
+  
+        <hr/>
+  
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/topics" component={Topics}/>
+      </div>
+    </Router>
+    );
+  }
+}
+
+export default App;
